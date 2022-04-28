f2a1d99e4d258b2c4ccc4da6b01aac72842f5e5b
majm <junmin.ma@shopee.com>
2022-04-28 23:58:28 +0800

    add: array

--- leetcode/leetcode_0088/leetcode_0088.go
+++ leetcode/leetcode_0088/leetcode_0088.go
@@ -5,29 +5,36 @@ package leetcode_0088
 
 //https://leetcode-cn.com/problems/merge-sorted-array/
 func merge(nums1 []int, m int, nums2 []int, n int) {
+
+	//如果 n == 0, 说明 nums2 已经插入完成, nums1 剩余的就是有序的
+	if n == 0 {
+		return
+	}
+
+	if m == 0 || nums1[m-1] <= nums2[n-1] {
+		nums1[m+n-1] = nums2[n-1]
+		merge(nums1, m, nums2, n-1)
+	} else {
+		nums1[m+n-1] = nums1[m-1]
+		merge(nums1, m-1, nums2, n)
+	}
+}
+
+func solution1(nums1 []int, m int, nums2 []int, n int) {
 	// checkParam
 	//
 	l := len(nums1)
 	idx := l - 1
 	for ; idx >= 0; idx-- {
-		if m == 0 || n == 0 {
+		if n == 0 {
 			break
 		}
-		if nums1[m-1] >= nums2[n-1] {
-			nums1[idx] = nums1[m-1]
-			m -= 1
-		} else {
+		if m == 0 || nums1[m-1] <= nums2[n-1] {
 			nums1[idx] = nums2[n-1]
-			n -= 1
+			n--
+		} else {
+			nums1[idx] = nums1[m-1]
+			m--
 		}
-	}
-
-	if n == 0 {
-		return
-	}
-	// 此时 idx == n-1
-	for idx >= 0 {
-		nums1[idx] = nums2[idx]
-		idx--
 	}
 }
--- leetcode/leetcode_0283/leetcode_0283.go
+++ leetcode/leetcode_0283/leetcode_0283.go
@@ -3,10 +3,34 @@
 // Date: 2022-04-17 17:14
 package leetcode_0283
 
-//双指针 解法
+func moveZeroes(nums []int) {
+	iterSolution(nums)
+}
+
+// solution 双指针
+func iterSolution(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+	// 指向第一个为0 的位置
+	left := 0
+	for i := range nums {
+		if nums[i] == 0 {
+			continue
+		}
+
+		if left != i {
+			nums[left] = nums[i]
+			nums[i] = 0
+		}
+		left++
+	}
+}
+
+//双指针 解法  swap
 // 左指针 左边 均为 非0
 // 右指针到左指针 处 均为 0
-func moveZeroes(nums []int) {
+func solutionSwap(nums []int) {
 	// 标记需要替换的位置
 	left, right := 0, 0
 	for right < len(nums) {
