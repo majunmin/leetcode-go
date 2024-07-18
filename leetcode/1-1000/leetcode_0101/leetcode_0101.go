59872acee0ac253d7446f8c4076f550238d682cc
majm <majm@ushareit.com>
2024-07-18 20:43:55 +0800

    [lcr]exercise [每日一题] fix

--- leetcode/1-1000/leetcode_0101/leetcode_0101.go
+++ leetcode/1-1000/leetcode_0101/leetcode_0101.go
@@ -6,9 +6,52 @@ package leetcode_0101
 import . "github.com/majunmin/leetcode-go/common"
 
 func isSymmetric(root *TreeNode) bool {
-	if root == nil || (root.Left == nil && root.Right == nil) {
+	// 方法二 迭代形式, 二叉树的层序遍历.
+	if root == nil {
+		return false
+	}
+	var (
+		queue1 = make([]*TreeNode, 0)
+		queue2 = make([]*TreeNode, 0)
+	)
+	queue1 = append(queue1, root)
+	queue2 = append(queue2, root)
+	for len(queue1) > 0 && len(queue2) > 0 {
+		if len(queue1) != len(queue2) {
+			return false
+		}
+		size := len(queue1)
+		for i := 0; i < size; i++ {
+			node1 := queue1[i]
+			node2 := queue2[i]
+			if node1 == nil && node2 == nil {
+				continue
+			}
+			if node1 == nil || node2 == nil || node1.Val != node2.Val {
+				return false
+			}
+			queue1 = append(queue1, node1.Left)
+			queue1 = append(queue1, node1.Right)
+			queue2 = append(queue2, node2.Right)
+			queue2 = append(queue2, node2.Left)
+		}
+		queue1 = queue1[size:]
+		queue2 = queue2[size:]
+	}
+	return true
+}
+
+// 1. 递归形式解法
+func recursiveSolution(root *TreeNode) bool {
+	return isSymmetricHelper(root, root)
+}
+
+func isSymmetricHelper(node1, node2 *TreeNode) bool {
+	if node1 == nil && node2 == nil {
 		return true
 	}
-	return isSymmetric(root.Left) && isSymmetric(root.Right) && (root.Left.Val == root.Right.Val)
-
+	if node1 == nil || node2 == nil {
+		return false
+	}
+	return node1.Val == node2.Val && isSymmetricHelper(node1.Left, node2.Right) && isSymmetricHelper(node1.Right, node2.Left)
 }
--- leetcode/1-1000/leetcode_0236/leetcode_0236.go
+++ leetcode/1-1000/leetcode_0236/leetcode_0236.go
@@ -4,7 +4,42 @@ import . "github.com/majunmin/leetcode-go/common"
 
 // https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	var (
+		parent  = make(map[*TreeNode]*TreeNode)
+		dfs     func(*TreeNode, *TreeNode)
+		visited = make(map[*TreeNode]bool)
+	)
 
+	dfs = func(node, pre *TreeNode) {
+		if node == nil {
+			return
+		}
+		parent[node] = pre
+		dfs(node.Left, node)
+		dfs(node.Right, node)
+	}
+	dfs(root, nil)
+	// 2. 构建路径
+	for p != nil {
+		visited[p] = true
+		p = parent[p]
+	}
+	for q != nil {
+		if visited[q] {
+			return q
+		}
+		q = parent[q]
+	}
+	return nil
+}
+
+// 方法二
+// 1. 通过一遍dfs遍历, 用一个 hash表 存储所有节点的父节点.
+// 2. 建立两条路径 p -> root 和 q -> root
+// 3. 两条路径的最早的交点就是  最近公共祖先节点
+
+// 1. 递归解法
+func solution1(root *TreeNode, p *TreeNode, q *TreeNode) *TreeNode {
 	// terminate
 	if root == nil {
 		return nil
@@ -15,7 +50,7 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	// process
 	lson := lowestCommonAncestor(root.Left, p, q)
 	rson := lowestCommonAncestor(root.Right, p, q)
-	if lson != nil || rson != nil {
+	if lson != nil && rson != nil {
 		return root
 	}
 	if lson == nil {
